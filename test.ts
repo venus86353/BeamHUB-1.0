1d5802683d3734ee5399a2e51b041d5f8e785c5c
venus86353 <103606120+venus86353@users.noreply.github.com>
2024-08-06 03:15:15 +0200

    Update and rename README.md to test.ts

---
+++ test.ts
@@ -0,0 +1,33 @@
+import { definePlugin } from "@utils/types";
+
+export default definePlugin({
+    name: "VoiceLoudness",
+    description: "Erhöht die maximale Lautstärke im Voice-Chat",
+    authors: [{ name: "YourName", id: 123456789n }],
+    
+    start() {
+        this.patchVoiceEngine();
+    },
+
+    stop() {
+        // Cleanup-Code hier, falls nötig
+    },
+
+    patchVoiceEngine() {
+        const VoiceEngine = BdApi.Webpack.getModule(m => m.prototype?.destroy && m.prototype.initialize);
+        BdApi.Patcher.after("VoiceLoudness", VoiceEngine.prototype, "initialize", (that) => {
+            if (that.voiceConnection) {
+                that.voiceConnection.setTransportOptions({
+                    input: {
+                        amplitudeThreshold: -100,
+                        energyThreshold: -100,
+                        automaticGainControl: false,
+                        noiseSuppression: false,
+                        echoCancellation: false,
+                        volumeMultiplier: 2 // Hier können Sie den Multiplikator anpassen
+                    }
+                });
+            }
+        });
+    }
+});
